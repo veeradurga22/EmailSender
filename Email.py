780ee12e0e6a7024a82c25d43c7492188e31cb1e
veeradurga22 <104752341+veeradurga22@users.noreply.github.com>
2023-03-06 19:50:03 +0530

    Update Email.py

--- Email.py
+++ Email.py
@@ -5,7 +5,7 @@ def EMailSend(toaddr,filename=None,subject="",b=""):
     from email.mime.text import MIMEText
     from email.mime.base import MIMEBase
     from email import encoders
-    fromaddr = "Enter user Email"                 #from address
+    fromaddr = "Enter ur Email"                 #from address
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
@@ -22,7 +22,7 @@ def EMailSend(toaddr,filename=None,subject="",b=""):
         msg.attach(p)
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    s.login(fromaddr, "Enter user Mail password here")
+    s.login(fromaddr, "Enter user gmail password here")
     text = msg.as_string()
     s.sendmail(fromaddr, toaddr, text)
     s.quit()
